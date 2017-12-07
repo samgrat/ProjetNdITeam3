f08eb33f40e4a31dd6de51fed553eb1828167471
Unknown <larnicol.titouan@orange.fr>
2017-12-07 21:45:28 +0100

    Check les mdp

--- NDI/app/inscription.php
+++ NDI/app/inscription.php
@@ -6,12 +6,11 @@ $loader = new Twig_Loader_Filesystem("views/");
 $twig = new Twig_Environment($loader,
 			      array("debug" => true));
 
-$args["text"] = "Hello";
-$args["login"] = "LOGIN";
 
 
-$page = "index";
 
-echo $twig->render("inscription.html", $args);
+$page = "inscription";
+
+echo $twig->render("inscription.html");
 
 ?>
--- NDI/app/js/inscription.js
+++ NDI/app/js/inscription.js
@@ -1,3 +1,8 @@
 function inscription(){
-	alert('hello');
+	var lsite_inscription = document.forms["formulaire"];
+	var mdp2 = document.forms["formulaire"].mdp.value;
+	var mdp1 = document.forms["formulaire"].mdp_confirm.value;
+	if( mdp1 != mdp2){
+		alert("mauvais mot de passe");
+	}
 }
