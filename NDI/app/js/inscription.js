aa9063b63f71f5e92c1131cf550b4e9cff8034f5
Unknown <larnicol.titouan@orange.fr>
2017-12-07 22:29:10 +0100

    Message

--- NDI/app/js/inscription.js
+++
@@ -1,8 +0,0 @@
-function inscription(){
-	var lsite_inscription = document.forms["formulaire"];
-	var mdp2 = document.forms["formulaire"].mdp.value;
-	var mdp1 = document.forms["formulaire"].mdp_confirm.value;
-	if( mdp1 != mdp2){
-		alert("mauvais mot de passe");
-	}
-}
