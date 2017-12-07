416bddce3ab8aefbdfa7c63453915950a9a43fb4
KmxCronos <joachim.ffde@gmail.com>
2017-12-07 22:36:50 +0100

    Update inscription.php

--- NDI/app/inscription.php
+++ NDI/app/inscription.php
@@ -6,11 +6,17 @@ $loader = new Twig_Loader_Filesystem("views/");
 $twig = new Twig_Environment($loader,
 			      array("debug" => true));
 
+$args["text"] = "Hello";
+$args["login"] = "LOGIN";
 
+$args = array();
+if(isset($_GET["error"])){
+	$args["message"] = $_GET["error"];
+}
+else if(isset($_GET["success"])){
+	$args["message"] = $_GET["success"];
+}
 
-
-$page = "inscription";
-
-echo $twig->render("inscription.html");
+echo $twig->render("inscription.html", $args);
 
 ?>
