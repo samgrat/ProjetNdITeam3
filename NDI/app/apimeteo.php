81d36d1ca5656e21f44084fb675438516c41d88d
Sam <smail.bamba@gmail.com>
2017-12-08 03:19:56 +0100

    Ca marche ! Affichage front a faire

--- NDI/app/apimeteo.php
+++ NDI/app/apimeteo.php
@@ -1,41 +1,99 @@
 <?php
 
-/**
- * infoclimat query function
- * @param string $urlcomponent (after the prefix)
- * @param array (associative) GET parameters (ex. ['language' => 'fr'])
- * @return string $content le tableau du climat sur 7 jours
-**/
-function infoget($long, $lat, $params=null) {
-    $apisufix = '&_auth=ABoAFwV7XX8EKVdgUiQHLlgwVGELfQEmAHwDYFg9AH1VPgNiVDQBZ18xVypUewcxUXwGZQE6UGBWPVUtDX8FZABqAGwFbl06BGtXMlJ9ByxYdlQ1CysBJgBiA2dYNQB9VTcDblQ3AX1fMlc8VHoHMVFmBmABIVB3VjRVNw1hBWQAZQBsBWNdPwRpVzdSfQcsWG5UZQs2ATAAMQMwWGEANlU%2BAzFUPwEwXzJXPFR6BzBRYwZmAT9Qa1YwVTYNZQV5AHwAHQUVXSIEK1d3UjcHdVh2VGELagFt&_c=2ec1f7704a082f63af6a553f7457f127';
-    $apicoord = $long . "," . $lat;
-    $apiprefix = 'http://www.infoclimat.fr/public-api/gfs/json';
-
-	  $targeturl = $apiprefix . '?_ll=' .  $apicoord . $apisufix;
-    $targeturl .= (isset($params) ? '&' . http_build_query($params) : '');
-    $stringcontent = file_get_contents($targeturl);
-    $content = json_decode($stringcontent, true);
-
-    return $content;
-}
-
 $lat =  43;
 $long = 0;
-$string2 = file_get_contents("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139");
-$string = "<iframe seamless width=\"888\" height=\"336\" frameborder=\"0\" src=\"http://www.infoclimat.fr/public-api/mixed/iframeSLIDE?_ll=". $lat .",". $long . "&_inc=WyJQYXJpcyIsIjQyIiwiMjk4ODUwNyIsIkZSIl0=&_auth=ABoAFwV7XX8EKVdgUiQHLlgwVGELfQEmAHwDYFg9AH1VPgNiVDQBZ18xVypUewcxUXwGZQE6UGBWPVUtDX8FZABqAGwFbl06BGtXMlJ9ByxYdlQ1CysBJgBiA2dYNQB9VTcDblQ3AX1fMlc8VHoHMVFmBmABIVB3VjRVNw1hBWQAZQBsBWNdPwRpVzdSfQcsWG5UZQs2ATAAMQMwWGEANlU%2BAzFUPwEwXzJXPFR6BzBRYwZmAT9Qa1YwVTYNZQV5AHwAHQUVXSIEK1d3UjcHdVh2VGELagFt&_c=2ec1f7704a082f63af6a553f7457f127\"></iframe>";
-;
+$string = file_get_contents("http://api.openweathermap.org/data/2.5/find?lat=".$lat."&lon=".$long."&APPID=777c795f08f824fa670b48a4b80019a6");
 
-var_dump($string2);
-    // TODO recuperer les info une a une
-/*$arrayinfo = infoget("48.866667", "2.333333");
-
-foreach ($arrayinfo as $key => $value) {
-  if(isset($value["temperature"])){
-    $temperature = $value["temperature"];
+$array = json_decode($string);
+foreach ($array as $key => $value) {
+  if( $key == "list"){
+    $list = $value;
   }
 }
 
-var_dump($temperature);
-var_dump($arrayinfo);
-*/
+foreach ($list[0] as $key2 => $value2) {
+
+  if($key2 == "name"){
+    $name = $value2;
+  }
+  if($key2 == "main"){
+    if($value2 != NULL){
+      $main = $value2;
+      foreach ($main as $key => $value) {
+        if($key == "temp"){
+          if($value != NULL){
+
+            $temperature = $value -273.15;
+          }else{ $temperature = False;}
+        }
+        if($key == "pressure"){
+          if($value != NULL){
+            $pression = $value;
+          }else{ $pression = False;}
+        }
+        if($key == "humidity"){
+          if($value != NULL){
+            $humidite = $value;
+          }else{ $humidite = False;}
+        }
+      }
+  }
+
+  }
+  if($key2 == "wind"){
+    if($value2 != NULL){
+      foreach ($value2 as $key3 => $value3) {
+        if($key3 == "speed"){
+          $vitessevent = $value3;
+        }
+      }
+
+    }else{$vitessevent = NULL;}
+
+    }
+  if($key2 == "rain"){
+    if($value2 != NULL){
+      $pluie = True;
+    } else{ $pluie = False;}
+  }
+  if($key2 == "snow"){
+    if($value2 != NULL){
+      $neige = True;
+    }else{ $neige = False;}
+  }
+  if($key2 == "clouds"){
+    if($value2 != NULL){
+      $nuageux = True;
+    }else{ $nuageux = False;}
+  }
+  if($key2 == "weather"){
+    if($value2 != NULL){
+      foreach ($value2 as $key3 => $value3) {
+        if($key3 == 0){
+          foreach ($value3 as $key4 => $value4) {
+            if($key4 == "main"){
+              $tempsprincipal = $value4;
+            }
+            if($key4 == "description"){
+              $description = $value4;
+            }
+
+          }
+        }
+      }
+    }else{ $tempsprincipal = NULL;
+    $description = NULL;}
+  }
+  }
+
+    var_dump($temperature);
+    var_dump($pression);
+    var_dump($humidite);
+    var_dump($vitessevent);
+    var_dump($pluie);
+    var_dump($neige);
+    var_dump($nuageux);
+    var_dump($tempsprincipal);
+    var_dump($description);
+
 ?>
