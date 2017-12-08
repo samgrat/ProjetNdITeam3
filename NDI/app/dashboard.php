c8127ba133be6f6f0cd63ae88f1640b8b5eaf490
Unknown <larnicol.titouan@orange.fr>
2017-12-08 01:13:26 +0100

    Soiree tab fonctionnel

--- NDI/app/dashboard.php
+++ NDI/app/dashboard.php
@@ -7,7 +7,11 @@ $twig = new Twig_Environment($loader,
 			      array("debug" => true));
 
 $args["login"] = "LOGIN";
-
+$args["tabSoiree"] = array(
+	array("nom" => "soire boloss", "date" => "2/12/2017"),
+	array("nom" => "soiree jeudi", "date" => "2/12/2017"),
+	array("nom" => "ricm party", "date" => "2/12/2017")
+);
 
 $page = "dashboard";
 
